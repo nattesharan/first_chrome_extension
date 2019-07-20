3802db17ffca4efe090b24b83bb50afeeea92c03
Natte Sharan <nattesharan@gmail.com>
2019-07-21 00:17:15 +0530

    initial commit

---
+++ background.js
@@ -0,0 +1,4 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // Send a message to the active tab
+    console.log("Callledddd");
+  });
---
+++ content.js
@@ -0,0 +1,2 @@
+console.log("Yeah it works!!!");
+// https://thoughtbot.com/blog/how-to-make-a-chrome-extension
