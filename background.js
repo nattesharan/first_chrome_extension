c544fe3f66a57c5a89d93b65d25b00ecd232c311
Natte Sharan <nattesharan@gmail.com>
2019-07-21 01:05:05 +0530

    changes

--- background.js
+++ background.js
@@ -1,4 +1,10 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     // Send a message to the active tab
     console.log("Callledddd");
+    let msg = {
+      'msg': "Helloooo"
+    };
+    chrome.tabs.sendMessage(tab.id, msg);
   });
+
+// browserAction.onClicked is not triggered if popup is there.
--- content.js
+++ content.js
@@ -1,2 +1,7 @@
 console.log("Yeah it works!!!");
 // https://thoughtbot.com/blog/how-to-make-a-chrome-extension
+
+function gotMessage(message, sender, reponse) {
+    console.log(message);
+}
+chrome.runtime.onMessage.addListener(gotMessage);
